ad18491a335d080668a76f47ed8613c1579dadba
segel <segel.jacob92@gmail.com>
2022-01-28 21:32:49 -0500

    user router in app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,16 @@
-
+import Vue from 'vue';
+import router from './router';
+import App from './components/App'
 
 require('./bootstrap');
-
-window.Vue = require('vue').default;
-
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 
 const app = new Vue({
     el: '#app',
+
+    component: {
+        App
+    },
+
+    router,
 });
